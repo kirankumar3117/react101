55f41f72937b7ead17322e97ede0ac76fa8487d1
kiran <ch.kirankumar311@gmail.com>
2022-05-16 22:50:59 +0530

    completed

---
+++ L1/reactnavbar/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./Navbar.css"
+export const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <div>
+        <h5>LOGOBAKERY</h5>
+      </div>
+      <div className='FLEX'>
+        <p>Services</p>
+        <p>Projects</p>
+        <p>About</p>
+      </div>
+     
+      
+       <button>Contact</button>
+     
+  
+
+    </div>
+  )
+}
