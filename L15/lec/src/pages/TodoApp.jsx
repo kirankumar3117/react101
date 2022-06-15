df544f05251bec3ca08e576c4bc3562fdfab79ca
kiran <ch.kirankumar311@gmail.com>
2022-06-15 20:00:30 +0530

    commited

--- L15/lec/src/pages/TodoApp.jsx
+++ L15/lec/src/pages/TodoApp.jsx
@@ -1,51 +1,52 @@
 import React from 'react'
 import {useRef} from 'react'
 import {useDispatch, useSelector} from 'react-redux'
-import { useEffect } from 'react'
+import {useEffect} from 'react'
 import {get, post} from '../store/actions'
 import axios from "axios"
 
 const TodoApp = () => {
     const ref = useRef()
     const dispatch = useDispatch();
-    const {todo :todo,loading,error} = useSelector((state) => state.todo.gettodo)
-    const {loading: addloadingpost}=useSelector((state)=>state.todo.posttodo)
-    
-   
-    const addNew=()=>{
-        
-        dispatch(post({
-            input:ref.current.value,
-            stat:false,
-          
-        }))
+    const {todo: todo, loading, error} = useSelector((state) => state.todo.gettodo)
+    const {loading: addloadingpost} = useSelector((state) => state.todo.posttodo)
+
+
+    const addNew = () => {
+
+        dispatch(post({input: ref.current.value, stat: false}))
     }
-  useEffect(()=>{
-      dispatch(get())
-  
-  },[])
-  console.log(todo)
+    useEffect(() => {
+        dispatch(get())
 
-  if(loading)  return <h1>Loading...</h1> 
-    return (<div>TodoApp
-        <input ref={ref}
-            type="text"/><button onClick={()=>{
-                addNew()
-            }}
-               
-          disabled={addloadingpost}>add</button>
+    }, [])
+    console.log(todo)
 
-        <br/>
-        <br/>
-        <br/> 
-        {
-        todo.map(e => {
-          
-            return <div> {
-                e.input
-            }</div>
-    })
-    } </div>)
+    if (loading) 
+        return <h1>Loading...</h1>
+    
+    return (
+        <div>TodoApp
+            <input ref={ref}
+                type="text"/><button onClick={
+                    () => {
+                        addNew()
+                    }
+                }
+
+                disabled={addloadingpost}>add</button>
+
+            <br/>
+            <br/>
+            <br/> {
+            todo.map(e => {
+
+                return <div> {
+                    e.input
+                }</div>
+        })
+        } </div>
+    )
 }
 
 
